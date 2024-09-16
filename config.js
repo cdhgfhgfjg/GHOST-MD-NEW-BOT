f71ed1f43161074e6370d0a84a2d097745291453
cdhgfhgfjg <pbmmalkanthi@gmail.com>
2024-09-16 10:26:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB:process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "Qq8Q1ZAR#eZLth4OG8MSNE98xPA4m-E-RE5I9UhwAxezOp7po2P4",
+MONGODB:process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 };
